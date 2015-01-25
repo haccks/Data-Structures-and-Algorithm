8ff46191ecf7c86e0f296c8604f7d5d39ccddba7
haccks <hacckspc@gmail.com>
2015-01-26 01:00:19 +0530

    Added deletion operations and corrected InsertEnd_SL function

--- Linked List/Single_Liked_List.c
+++ Linked List/Single_Liked_List.c
@@ -11,6 +11,10 @@ void InsertFront_SL(struct Node **, int );
 void InsertEnd_SL(struct Node *, int );
 void InsertAny_SL(struct Node *, int , int );
 void print_SL(struct Node *);
+void DeleteFront_SL(struct Node ** );
+void DeleteEnd_SL(struct Node * );
+void DeleteAny_SL(struct Node **, int );
+
 
 int main(void)
 {
@@ -23,7 +27,9 @@ int main(void)
 		printf("2. Insert node at the end\n");
 		printf("3. Insert note at any position\n");
 		printf("4. Print Linked list\n");
-		
+		printf("5. Delete node from beginning\n");
+		printf("6. Delete node from end\n");
+		printf("7. Delete node from any position\n");
 		printf("Press -1 to exit\n\n");
 		
 		int op, val, key;
@@ -48,7 +54,14 @@ int main(void)
 			case 4: printf("Linked list is: ");
 					print_SL(list);
 					break;
-							
+			case 5: DeleteFront_SL(&list); 	break;
+			
+			case 6: DeleteEnd_SL(list); 	break;
+			
+			case 7: printf("Enter a key data of the node which is to be deleted: ");
+					scanf("%d", &key);
+					DeleteAny_SL(&list, key);
+					break;				
 			case -1: exit(0);
 			
 			default: printf("Wrong choice");
@@ -81,13 +94,14 @@ void InsertEnd_SL(struct Node *list, int val)
 		return;
 	}
 	
-	while(list)
+	while(list->next != NULL)
 		list = list->next;
 	 	
 	new->data =  val;
-	list = new;
+	list->next = new;
 	new->next = NULL;
 }
+
 void InsertAny_SL(struct Node *list, int val, int key)
 {
 	struct Node *new = malloc(sizeof(struct Node));
@@ -119,4 +133,66 @@ void print_SL(struct Node *list)
 		list = list->next;
 	}
 	putchar('\n');
+}
+
+void DeleteFront_SL(struct Node **list)
+{
+	struct Node *ptr = *list;
+	
+	if(ptr == NULL)
+	{
+		printf("The list is empty: No deletion\n");
+		return;
+	}
+	
+	*list = (*list)->next;
+	//ptr->next = NULL;
+	free(ptr);
+}
+
+void DeleteEnd_SL(struct Node *list)
+{
+	struct Node *prev = NULL;
+	if(list == NULL)
+	{
+		printf("The list is empty: No deletion\n");
+		return;
+	}
+	
+	while(list->next != NULL)
+	{
+		prev = list;
+		list = list->next;
+	}
+	if(prev != NULL)
+		prev->next = NULL;		
+	free(list);
+}
+
+void DeleteAny_SL(struct Node **list, int key)
+{
+	struct Node *prev = NULL;
+	struct  Node *cur = *list;
+	if(list == NULL)
+	{
+		printf("The list is empty: No deletion\n");
+		return;
+	}
+	
+	while(cur != NULL && cur->data != key)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+	
+	if(cur == NULL)
+	{
+		printf("Node with key does not exist\n");
+		return;
+	}	
+	else if(prev == NULL)
+		*list = (*list)->next;
+	else
+		prev->next = cur->next;	
+	free(cur);
 }
